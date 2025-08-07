9d8aa99eb3e1c36c7a6e44cc410cddfee121329a
Daniel88V <daniel12velo@gmail.com>
2025-08-06 19:02:21 -0600

    Actividad12

--- Actividad12.py
+++ Actividad12.py
@@ -1,12 +1,3 @@
-def busqueda_por_valor(lista, objetivo):
-    for elemento in lista:
-        if elemento == objetivo:
-            return elemento
-    return None
-datos = [5, 8, 12, 7, 3, 9, 100]
-valor_a_buscar = int(input("Ingresa el número a buscar: "))
-resultado = busqueda_por_valor(datos, valor_a_buscar)
-if resultado is not None:
-    print(f"El valor {resultado} está en la lista.")
-else:
-    print(f"el valor {valor_a_buscar} no está en la lista.")
+repartidores = {}
+cont = int(input("Ingrese cuantos repartidores trabajaron hoy: "))
+for i in range(cont):
